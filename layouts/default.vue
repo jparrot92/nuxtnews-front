da77ededbcd0c6d98a9ddc1aad948480c37cc937
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-11-01 11:09:48 +0100

    Ajustar el layout de nuestra aplicación

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,86 +6,136 @@
       :clipped="clipped"
       fixed
       app
+      color="primary"
+      dark
     >
       <v-list>
+        <!-- RUTAS PÚBLICAS -->
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
+          v-for="nav in publicNavs"
+          :key="nav.to"
+          :to="nav.to"
           router
           exact
         >
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>{{ nav.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title v-text="nav.title" />
           </v-list-item-content>
         </v-list-item>
+        <v-divider />
+        <!-- ./RUTAS PÚBLICAS -->
+
+        <!-- LOGIN -->
+        <v-list-item v-if="!isLogged" to="/acceso">
+          <v-list-item-action>
+            <v-icon>fas fa-key</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Acceder'" />
+          </v-list-item-content>
+        </v-list-item>
+        <!-- ./LOGIN -->
+
+        <!-- LOGOUT -->
+        <v-list-item v-else @click="logout">
+          <v-list-item-action>
+            <v-icon>fas fa-sign-out-alt</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Salir'" />
+          </v-list-item-content>
+        </v-list-item>
+        <!-- ./LOGOUT -->
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="blue-grey darken-4"
+      dark
+    >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon
+          >fas fa-chevron-{{ `${miniVariant ? 'right' : 'left'}` }}</v-icon
+        >
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
+        <v-icon
+          >fas fa-{{
+            `${clipped ? 'hand-point-right' : 'hand-point-left'}`
+          }}</v-icon
+        >
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
+        <v-icon>fas fa-menu</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-main>
-      <v-container>
+    <v-content>
+      <v-container fluid>
         <nuxt />
       </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>NuxtNews &copy; 2020</span>
     </v-footer>
+
+    <!-- NO TIENE PERMISOS -->
+    <v-snackbar v-model="unauthorized" :timeout="2000">
+      No tienes permiso para realizar esa operación
+    </v-snackbar>
+    <!-- ./NO TIENE PERMISOS -->
   </v-app>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
 export default {
   data() {
     return {
       clipped: false,
-      drawer: false,
+      drawer: true,
       fixed: false,
-      items: [
+      publicNavs: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'fas fa-home',
+          title: 'Inicio',
           to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          icon: 'fas fa-list',
+          title: 'Posts',
+          to: '/posts',
         },
       ],
-      miniVariant: false,
+      miniVariant: true,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'NuxtNews',
     }
+  },
+  computed: {
+    ...mapGetters('authentication', ['isLogged']),
+    ...mapState(['unauthorized']),
+  },
+  methods: {
+    async logout() {
+      try {
+        const Cookie = process.client ? require('js-cookie') : undefined
+        Cookie.set('auth', null)
+        this.$store.commit('authentication/SET_AUTH', null)
+        await this.$router.push('/acceso')
+      } catch (e) {
+        console.log(e)
+      }
+    },
   },
 }
 </script>
